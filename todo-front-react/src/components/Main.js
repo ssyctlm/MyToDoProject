bb80211328b78f9a9dbedf85f8571c5344d0c5d3
hellow <myqy480@gmail.com>
2020-08-21 18:04:35 +0800

    totally messy

--- todo-front-react/src/components/Main.js
+++ todo-front-react/src/components/Main.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Main.css';
+import Task from './Task';
 // import {Container, Row, Col} from 'react-bootstrap';
 class Main extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      todoList:['wash plates','do the laundry']
+      todoList:[
+        {id:1, finish:false, text:"wash plates"},
+        {id:2, finish:false, text:"do the laundry"},
+        {id:3, finish:false, text:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt placeat nobis voluptas iure animi tenetur sit dignissimos aperiam excepturi ipsa veritatis amet non, velit nemo, tempora facilis inventore dolore deleniti!"}]
     }
   }
+  // finish = (id)=>{
+  //   this.setState(state =>({
+  //     flag: !state.flag
+  //   }))
+
+  // }
   render(){
+    
     return (
       <container className="fluid">
         <div className="row">
@@ -17,9 +29,12 @@ class Main extends React.Component{
           </div>
           <div className="col-12 bg-light" style={{height:"88vh"}}>
             <div className="row justify-content-center" style={{height:"60vh"}}>
-              <ul>
-              <li>{ this.state.todoList.map(e=>e)}</li>
+              <div className="col-10 my-5">
+              <ul className="list-group">
+
+              { this.state.todoList.map(task =><Task key={task.id} inbox={task.text} index={task.id}/>) }
               </ul>
+              </div>
             </div>
             <div className="row justify-content-center">
                  <div className="input-group input-group-lg col-10 col-md-6 mb-4">
